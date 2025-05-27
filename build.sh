c37cd5a3d9e6fef7ea9ee4b414c2e290133a1767
itachiccc <itachiccc@users.noreply.github.com>
2025-05-27 11:36:26 +0800

    Update build.sh
    Dynamically load the defconfig file.

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ echo -e " $yellow #####|       top of the script to enable KernelSU patches   |#
 
 # ---------------------------------------------------------------------------------------------------------------- #
 # ---------------------------| EXPORTS and Directory Setup |------------------------------------------------------ #
-KERNEL_DEFCONFIG=gki_defconfig  # Looks for defconfig in arch/<exported_arch>/configs/
+KERNEL_DEFCONFIG=${1:-gki_defconfig}  # Looks for defconfig in arch/<exported_arch>/configs/
 ANYKERNEL3_DIR=$PWD/AnyKernel3/ # Required by the function zip_kernel
 CLANG_VERSION=clang-r547379
 CLANG_DIR="$HOME/Git/Clang/$CLANG_VERSION"
